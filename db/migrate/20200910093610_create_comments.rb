8cd1a9f358be30dcb1784c3c213adf54945ecfdb
Vitalie Melnic <vmwhoami@gmail.com>
2020-09-10 12:55:12 +0300

    Add comments table and links

---
+++ db/migrate/20200910093610_create_comments.rb
@@ -0,0 +1,13 @@
+class CreateComments < ActiveRecord::Migration[5.2]
+  def change
+    create_table :comments do |t|
+      t.text :comment
+      t.references :user , foreign_key: true
+      t.references :post , foreign_key: true
+
+
+      t.timestamps
+    end
+  
+  end
+end
